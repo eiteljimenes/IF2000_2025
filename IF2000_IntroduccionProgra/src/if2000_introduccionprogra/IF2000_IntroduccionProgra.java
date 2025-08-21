7e952ded05bbe2d310b3caf2e878b4be6fef0484
eiteljimenez <eiteljimenes@gmail.com>
2025-08-21 11:08:39 -0600

    https://github.com/eiteljimenes/IF2000_2025.git

--- IF2000_IntroduccionProgra/src/if2000_introduccionprogra/IF2000_IntroduccionProgra.java
+++ IF2000_IntroduccionProgra/src/if2000_introduccionprogra/IF2000_IntroduccionProgra.java
@@ -1,39 +1,58 @@
-package if2000_introduccionprogra;
-import domain.Person;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 
 /**
  *
  * @author estudiante
  */
-public class IF2000_IntroduccionProgra {
 
+package if2000_introduccionprogra;
+ import Logic.*;
+
+public class IF2000_IntroduccionProgra {
+    
+    
+   
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-   
-    String name = "Lionel";
+  // Person p = new Person ("Manuel","Jimenez","C234432","E772812",20);
+ Repositorio_semana2_practica1 ej = new Repositorio_semana2_practica1();
+  ej.quiz2_B_piramide(4);
+    
+    }}
+
+    
+/* String name = "Lionel";
     int age =40;
     
     System.out.println("Hello Word ");
     System.out.println (" name: "+name);
     System.out.println(" Age: "+age);
-        /*
+  
+    }
+}   /*
         Tipos de datos Numericos:
         int,long,short,float,double,numeric,money,decimal
         Manejo de fechas: Date
         Cadena de texto: Strng
         Objetos: object
         Data structres:
-        Arrays or Vectors: int[] x;
-        int[][] y;
+        Arrays or Vectors: int[] x; 
+          int[][] y;
          */    
            
     
-Person P = new Person ("Manuel","Jimenez","C234432","E772812",20);
+
     
-    }
     
-}
+
+ 
+
+
+    
 
